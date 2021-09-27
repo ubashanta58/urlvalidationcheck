5d369a5358cc24a4e522f150720a4949040c48b2
Niraj Paudel <itsmenirajpaudel@gmail.com>
2021-09-27 11:28:21 +0545

    Make validationchecker function little better

--- app.js
+++ app.js
@@ -1,67 +1,81 @@
-const fs = require('fs');
-const csv = require('csv-parser');
+const fs = require("fs");
+const csv = require("csv-parser");
 const updatedWebsiteURL = [];
 
 /**
- * 
- * @param {string} str 
- * @returns 
+ *
+ * @param {string} str
+ * @returns
  */
-function validationChecker(str){
-    if(!str || str.length === 0 || str.includes('https://') || str.includes('https://www') || str.includes('http://')){
-        return false;
-    }else {
-        return true;
+function validationChecker(str) {
+  let returnVal = false;
+  if (
+    !str ||
+    str.length === 0 ||
+    str.includes("https://") ||
+    str.includes("https://www") ||
+    str.includes("http://")
+  ) {
+    returnVal = true;
+  }
+  return returnVal;
+}
+
+/**
+ *
+ * @param {string} str
+ * @returns
+ */
+function makeValidUrl(str) {
+  let correctURL = new String("");
+  if (
+    !str.includes("www") &&
+    !str.includes("https://") &&
+    !str.includes("https://www.") &&
+    !str.includes("http://")
+  ) {
+    correctURL = "https://www." + str;
+  } else {
+    correctURL = "https://" + str;
+  }
+
+  return correctURL;
+}
+
+/**
+ * writeFileToCSV
+ */
+function writeToCSVFile() {
+  const fileName = "updated-websites-URL.csv";
+  fs.writeFile(fileName, extractAsCSV(), (err) => {
+    if (err) {
+      console.log("Error writing to CSV file", err);
+    } else {
+      console.log(`saved as ${fileName}`);
     }
+  });
 }
 
 /**
- * 
- * @param {string} str 
- * @returns 
- */
-function makeValidUrl(str){
-    let correctURL = new String("");
-    if(!str.includes('www') && !str.includes('https://') && !str.includes('https://www.') && !str.includes('http://')){
-        correctURL = 'https://www.' + str;
-    }else {
-        correctURL = 'https://' + str;
-    }
-    
-    return correctURL;
-}
-
-/**
- * writeFileToCSV 
- */
-function writeToCSVFile(){
-    const fileName = 'updated-websites-URL.csv'
-    fs.writeFile(fileName, extractAsCSV(), err => {
-        if(err){
-            console.log('Error writing to CSV file', err);
-        } else {
-            console.log(`saved as ${fileName}`);
-        }
-    })
-}
-
-/**
- * 
+ *
  * @returns concat header and updated URL with "\n"
  */
-function extractAsCSV(){
-    const header = ['Updated Url'];
-    return header.concat(updatedWebsiteURL).join("\n");
+function extractAsCSV() {
+  const header = ["Updated Url"];
+  return header.concat(updatedWebsiteURL).join("\n");
 }
 
 /**
  * read CSV file and on data check the validation and update, on end write file to CSV
  */
-fs.createReadStream('business.csv').pipe(csv()).on('data', (row) => {
-    if(validationChecker(row.website)){
-       row.website =  makeValidUrl(row.website);
-       updatedWebsiteURL.push(row.website);
+fs.createReadStream("business.csv")
+  .pipe(csv())
+  .on("data", (row) => {
+    if (!validationChecker(row.website)) {
+      row.website = makeValidUrl(row.website);
+      updatedWebsiteURL.push(row.website);
     }
-}).on('end', () => {
+  })
+  .on("end", () => {
     writeToCSVFile();
-})
+  });
